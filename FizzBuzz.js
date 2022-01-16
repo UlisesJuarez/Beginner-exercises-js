fb3c8f4f38e1bce32b0619e879430267c0e9a0eb
UlisesJE <ujuareze1700@alumno.ipn.mx>
2022-01-15 22:15:01 -0600

    buzzy update

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -2,32 +2,19 @@ var output=[];
 var count=1;
 
 function fizzBuzzy(){
-
-    if (count%3===0 && count%5===0){
-        output.push("FizzBuzz")
-    }else if (count%3===0){
-        output.push("Fizz")
-    }else if (count%5===0){
-        output.push("Buzz")
-    }else{
-        output.push(count)
+    while(count<101){
+        if (count%3===0 && count%5===0){
+            output.push("FizzBuzz")
+        }else if (count%3===0){
+            output.push("Fizz")
+        }else if (count%5===0){
+            output.push("Buzz")
+        }else{
+            output.push(count)
+        }
+        count++;
     }
-    count++;
+
     document.write(output+"<br>")
 }
-
-fizzBuzzy();
-fizzBuzzy();
-fizzBuzzy();
-fizzBuzzy();
-fizzBuzzy();
-fizzBuzzy();
-fizzBuzzy();
-fizzBuzzy();
-fizzBuzzy();
-fizzBuzzy();
-fizzBuzzy();
-fizzBuzzy();
-fizzBuzzy();
-fizzBuzzy();
 fizzBuzzy();
